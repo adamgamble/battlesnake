33a7750ce2e19817cdb9c6d49311ce9e09600a3d
Adam Gamble <adamgamble@Adams-MacBook-Pro.local>
2021-08-27 15:00:30 -0500

    Started ranking

--- app/objects/move_planner.rb
+++ app/objects/move_planner.rb
@@ -5,31 +5,39 @@ class MovePlanner
   end
 
   def next_move
+    moves = possible_moves
+    possible_moves.each do |move|
+      puts "------------------"
+      puts move.inspect
+      puts "------------------"
+    end
+    moves = rank_moves(moves) 
+    move = possible_moves.first.first
+    puts "Moving #{move.to_s}"
+    move
+  end
+
+  def possible_moves
     spaces = @game_object.map.open_spaces_from_coordinates(@game_object.me.head[:x], @game_object.me.head[:y])
     food = spaces.select { |arrow, space| space[:food] }
     open = spaces.select { |arrow, space| space[:open] }
     possible_moves = []
     if food.any?
-      food.each do |arrow, space|
-        next_spaces = @game_object.map.open_spaces_from_coordinates(space[:x], space[:y])
-        space[:next_move_count] = next_spaces.keys.count
-      end
-      possible_food = food.map {|arrow, space| [arrow, space[:next_move_count]]}
-      possible_food.sort! {|a,b| b.last <=> a.last}
+      possible_food = food.map {|arrow, space| [arrow, space] }
       possible_moves += possible_food
-    else
-      open.each do |arrow, space|
-        next_spaces = @game_object.map.open_spaces_from_coordinates(space[:x], space[:y])
-        space[:next_move_count] = next_spaces.keys.count
-      end
-      possible_open = open.map {|arrow, space| [arrow, space[:next_move_count]]}
-      possible_moves += possible_open
     end
-    possible_moves.uniq!
-    possible_moves.sort! {|a,b| b.last <=> a.last}
-    puts "Possible Moves: #{possible_moves.map(&:first).join(",")}"
-    move = possible_moves.first.first
-    puts "Moving #{move.to_s}"
-    move
+    if open.any?
+      possible_open = open.map {|arrow, space| [arrow, space] }
+      possible_moves += possible_open.shuffle
+    end
+    possible_moves.uniq
+  end
+
+  def score_moves possible_moves
+    # [:left, {:x=>2, :y=>3, :open=>true, :food=>false}]
+    possible_moves.each do |possible_move|
+      
+    end
+
   end
 end
